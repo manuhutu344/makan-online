1f8e1f607b7273f1f9f8abc8204346944110e007
manuhutu344 <nandoperdos2@gmail.com>
2023-07-20 10:39:24 +0900

    Membuat tampilan awal navbarnya

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import navbar from './navbar.css'
+import classes from './navbar.module.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={classes.container}>
+    <div className={classes.wrapper}>
+    <Link href='/' className={classes.left}>
+    <h2>Makan Online</h2>
+    </Link>
+    <ul className={classes.center}>
+    <li className={classes.listItem}>Home</li>
+    <li className={classes.listItem}>About</li>
+    <li className={classes.listItem}>Konatak Kami</li>
+    <li className={classes.listItem}>unggulan</li>
+    </ul>
+    <div className={classes.right}>
+    <AiOutlineMail size={30} />
+    <button className={classes.signIn}>Sign In</button>
+    </div>
+    </div>
+    </div>
   )
 }
 

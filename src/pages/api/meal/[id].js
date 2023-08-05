123d500cc7c958b8bd563c91ef7795d4df2d186e
manuhutu344 <nandoperdos2@gmail.com>
2023-08-05 18:38:33 +0900

    Coba

--- src/pages/api/meal/[id].js
+++ src/pages/api/meal/[id].js
@@ -13,10 +13,5 @@ export default async function handler(req, res){
             const meal = await Makanan.findByIdAndDelete(req.query.id)
             return res.status(200).json(meal)
         }
-        case "PUT":{
-            const {title, desc, category, price, image, featured} = (req.body)
-            const makan = await Makanan.findByIdAndUpdate(req.query.id, {title, desc, category, price, image, featured})
-            return res.status(200).json(makan)
-        }
     }
 }
